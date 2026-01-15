0993ce7715c00d4b1d9dd1d369c4b347bb1f1627
shahd <mazouzchahd12@gmail.com>
2026-01-15 11:06:11 +0100

    fix a bug in require file

--- app/core/Database.php
+++ app/core/Database.php
@@ -3,7 +3,7 @@
 class Database {
     public static function connect(){
         try {
-            $config = require_once __DIR__ . '../config/database.php';
+            $config = require_once __DIR__ . '/../../config/database.php';
             $db = new PDO(
                 "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}", $config['user'], $config['password']);
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
