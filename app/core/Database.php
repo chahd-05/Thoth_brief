c2523f2064f1213896d7071be1acee8daf8d837c
shahd <mazouzchahd12@gmail.com>
2026-01-15 10:23:42 +0100

    feat: connect database

---
+++ app/core/Database.php
@@ -0,0 +1,17 @@
+<?php
+
+class Database {
+    public static function connect(){
+        try {
+            $config = require_once __DIR__ . '../config/database.php';
+            $db = new PDO(
+                "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}", $config['user'], $config['password']);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e){
+            die("error database: " . $e->getMessage());
+        }
+      
+    }
+}
+?>
---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'host'=>'localhost',
+    'user'=>'root',
+    'port'=>'3307',
+    'dbname'=>'thoth',
+    'password'=>''
+];
+
+?>
