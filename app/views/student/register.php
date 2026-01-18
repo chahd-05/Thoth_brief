eea45c9cddfe5aa20f9f91e04cdf5f736d4dc933
shahd <mazouzchahd12@gmail.com>
2026-01-18 19:28:33 +0100

    create register page

---
+++ app/views/student/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="/thoth_brief/public/css/style.css">
+</head>
+<body>
+    <h2>Register</h2>
+    <?php if(!empty($error)): ?>
+        <p style="color: red"><?= $error ?></p>
+        <?php endif; ?>
+
+        <form method="POST">
+            <input type="text" name="name" placeholder="enter your name" required>
+            <input type="email" name="email" placeholder="enter your email" required>
+            <input type="password" name="password" placeholder="password" required>
+            <button type="submit">Register</button>
+            <a href="/thoth_brief/public/student/login">Login</a>
+        </form>
+        
+</body>
+</html>
