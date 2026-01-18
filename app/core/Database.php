cba37cbe67d4f87eccca233a9b7ec20d7cced46e
shahd <mazouzchahd12@gmail.com>
2026-01-18 19:27:17 +0100

    create database connection

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,12 +1,16 @@
 <?php
 
 class Database {
-    public static function connect(){
+    public static function getConnection(){
+
+    $config = require __DIR__ . '/../../config/database.php';
+
         try {
-            $config = require_once __DIR__ . '/../../config/database.php';
+            
             $db = new PDO(
                 "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}", $config['user'], $config['password']);
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $db;
         }
         catch(PDOException $e){
             die("error database: " . $e->getMessage());
